272cc4af3234102ef99ba695d2767851e5e44845
James Baskerville <baskerville@college.harvard.edu>
2016-11-30 01:36:29 -0500

    working on restorenet

--- restorenet.py
+++ restorenet.py
@@ -1,8 +1,54 @@
 import tensorflow as tf
 
-sess = tf.Session()
-new_saver = tf.train.import_meta_graph('chess-ann.meta')
-new_saver.restore(sess, 'chess-ann.data-00000-of-00001')
-all_vars = tf.trainable_variables()
+# Parameters
+learning_rate = 0.05
+training_epochs = 3
+display_step = 1
+batch_size = 100
 
+# Network Parameters
+n_hidden_1 = 256 # 1st layer number of features
+n_hidden_2 = 256 # 2nd layer number of features
+n_input = 65 # Chess position array: 64 squares + board.turn
+n_classes = 200 # Possible engine scores (-10,000 < score < 10,000)
+
+# tf Graph input
+x = tf.placeholder("float", [None, n_input])
+y = tf.placeholder("float", [None, n_classes])
+
+# Create model
+def multilayer_perceptron(x, weights, biases):
+    # Hidden layer with RELU activation
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.relu(layer_1)
+    # Hidden layer with RELU activation
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)
+    # Output layer with linear activation
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    return out_layer
+
+# Store layers weight & bias
+weights = {
+    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
+}
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
+# Construct model
+pred = multilayer_perceptron(x, weights, biases)
+
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    # restore variables from disk
+    saver.restore(sess, "./chess-ann.ckpt")
+    print "Model restored"
+
+    tf.argmax(pred, 1)
 
