1b77f4ddd6ce5531e469429f1ec53143633eb5ed
William Long <wlong0827@gmail.com>
2016-11-30 00:33:43 -0500

    neural net working

--- chessplayer.py
+++ chessplayer.py
@@ -16,23 +16,6 @@ import time
 import os
 import sys
 
-def startProgress(title):
-    global progress_x
-    sys.stdout.write(title + ": [" + "-"*40 + "]" + chr(8)*41)
-    sys.stdout.flush()
-    progress_x = 0
-
-def progress(x):
-    global progress_x
-    x = int(x * 40 // 100)
-    sys.stdout.write("#" * (x - progress_x))
-    sys.stdout.flush()
-    progress_x = x
-
-def endProgress():
-    sys.stdout.write("#" * (40 - progress_x) + "]\n")
-    sys.stdout.flush()
-
 class ChessPlayer:
     """
     The ChessPlayer class contains all of the chess engines
